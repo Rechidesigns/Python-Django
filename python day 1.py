49bffdd6ffa0bc6e517f02c6d83bd53247d1b963
Rechidesigns <alerechiordu@gmail.com>
2022-06-27 15:57:03 +0100

    python classes at univelcity

---
+++ python day 1.py
@@ -0,0 +1,23 @@
+print("My name is Alerechi")
+print("4") #this is an integer
+print("4.5") #this is a floot
+print("True") #this is a boolean
+
+name = "Alerechi"
+
+print(name)
+
+num1 = 15
+num2 = 35
+
+print(num1*num2)
+
+a = 4
+b = 7
+c = 3
+   
+numerator = -b + ((b**2) - (4*a*c))**0.5
+denomiator = 2*a
+
+x = numerator/denomiator
+print(x)
